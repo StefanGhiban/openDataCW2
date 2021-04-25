4069f81b7af4520e1ec72968cc75ad72467c0bcf
StefanGhiban <stefanghiban962@gmail.com>
2021-04-25 13:42:44 +0300

    added some packages and deleted default content from the main app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "react-bootstrap/Button";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1">Page1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/page1">
+            <div>
+              <Button>Some button</Button>
+            </div>
+          </Route>
+          <Route path="/page2">
+            <div>
+              someososmsosmos
+            </div>
+          </Route>
+          <Route path="/">
+            <div>
+              contenttt
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
