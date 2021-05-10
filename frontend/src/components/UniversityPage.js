35f8c1921e85175ee5f5afd342e91bd19b5fda74
iustin.dinca005@gmail.com <iustin.dinca005@gmail.com>
2021-05-10 16:34:36 +0300

    working version for presentation

--- frontend/src/components/UniversityPage.js
+++ frontend/src/components/UniversityPage.js
@@ -40,11 +40,15 @@ const UniversityPage = () => {
           <Col>
               <Jumbotron className="d-flex flex-column align-items-center jumbo">
                   <h1>{state.university.title}</h1>
-                  <p>
-                      World ranking: {state.university.worldRanking} / UK ranking: {state.university.ukRanking}
-                  </p>
-                  <h4>
-                    Overall Score: {state.university.overallScore}
+                  <h3 className="mb-1">
+                      World ranking: {state.university.worldRanking}
+                  </h3>
+                  <h3 className="mb-1">
+                      UK ranking: {state.university.ukRanking}
+                  </h3>
+
+                  <h4 className="mb-1 ">
+                      Overall Score: {state.university.overallScore}
                   </h4>
               </Jumbotron>
           </Col>
@@ -53,7 +57,7 @@ const UniversityPage = () => {
         <Container>
           <Row>
             <Col className="d-flex justify-content-center flex-column align-items-center">
-              <h3>University scores <i class="fas fa-university"></i></h3>
+              <h3 className="pb-3">University scores <i class="fas fa-university"></i></h3>
               <div className="d-flex justify-content-center align-items-center">
                 <ListGroup className="px-2" variant="flush">
                   <ListGroup.Item>Teaching score: {state.university.teachingScore}</ListGroup.Item>
@@ -71,71 +75,67 @@ const UniversityPage = () => {
           <hr className="py-2"/>
           <Row>
             <Col className="d-flex justify-content-center flex-column align-items-center">
-              <h3>City prices <i class="fas fa-pound-sign"></i></h3>
-              <Container fluid className="bg-primary">
+              <h3 className="pb-3">City prices <i class="fas fa-pound-sign"></i></h3>
+                <Container fluid>
+                {/* <Carousel>
+                  <Carousel.Item>
+                    <h3>Groceries <i class="fas fa-shopping-cart"></i></h3>
+                    <ListGroup className="px-5" variant="flush">
+                      {
+                        prices.map(price => {
+                          if(price.item_name === "Eggs (regular) (12), Markets" ||
+                          price.item_name === "Milk (regular), (1 liter), Market"||
+                          price.item_name === "Meal, Inexpensive Restaurant, Restaurants"||
+                          price.item_name === "McMeal at McDonalds (or Equivalent Combo Meal), Restaurants"||
+                          price.item_name === "Local Cheese (1kg), Markets"||
+                          price.item_name === "Chicken Fillets (1kg), Markets"
+
+                          ) {
+                            return <ListGroup.Item>{price.item_name}  {(Math.round(price.average_price*100) /100).toFixed(2)}£</ListGroup.Item>
+                          }
+                        })
+                      }
+                    </ListGroup>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <h3>Rent <i class="fas fa-home"></i></h3>
+                    <ListGroup className="px-5" variant="flush">
+                    {
+                        prices.map(price => {
+                          if(price.item_name === "Apartment (1 bedroom) Outside of Centre, Rent Per Month" ||
+                          price.item_name === "Apartment (1 bedroom) in City Centre, Rent Per Month" ||
+                          price.item_name === "Basic (Electricity, Heating, Cooling, Water, Garbage) for 85m2 Apartment, Utilities (Monthly)"||
+                          price.item_name === "Apartment (3 bedrooms) in City Centre, Rent Per Month"||
+                          price.item_name === "Apartment (3 bedrooms) Outside of Centre, Rent Per Month"||
+                          price.item_name === "Internet (60 Mbps or More, Unlimited Data, Cable/ADSL), Utilities (Monthly)"
+                          ) {
+                            return <ListGroup.Item class="d-flex justify-content-between"><span>{price.item_name}</span>  <span>{(Math.round(price.average_price*100) /100).toFixed(2)}£</span></ListGroup.Item>
+                          }
+                        })
+                      } 
+                    </ListGroup>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <h3>Transportation <i class="fas fa-shopping-cart"></i></h3>
+                    <ListGroup className="px-5" variant="flush">
+                      {
+                        prices.map(price => {
+                          if(
+                          price.item_name === "One-way Ticket (Local Transport), Transportation"||
+                          price.item_name === "Monthly Pass (Regular Price), Transportation"||
+                          price.item_name === "Gasoline (1 liter), Transportation"||
+                          price.item_name === "Volkswagen Golf 1.4 90 KW Trendline (Or Equivalent New Car), Transportation"
+
+                          ) {
+                            return <ListGroup.Item>{price.item_name} {(Math.round(price.average_price*100) /100).toFixed(2)}£</ListGroup.Item>
+                          }
+                        })
+                      }
+                    </ListGroup>
+                  </Carousel.Item>
+                </Carousel> */}
                 <Row>
-                  <Col>
-                    <Carousel>
-                      <Carousel.Item>
-                        <h3>Groceries <i class="fas fa-shopping-cart"></i></h3>
-                        <ListGroup className="px-5" variant="flush">
-                          {
-                            prices.map(price => {
-                              if(price.item_name === "Eggs (regular) (12), Markets" ||
-                              price.item_name === "Milk (regular), (1 liter), Market"||
-                              price.item_name === "Meal, Inexpensive Restaurant, Restaurants"||
-                              price.item_name === "McMeal at McDonalds (or Equivalent Combo Meal), Restaurants"||
-                              price.item_name === "Local Cheese (1kg), Markets"||
-                              price.item_name === "Chicken Fillets (1kg), Markets"
-
-                              ) {
-                                return <ListGroup.Item>{price.item_name} {(Math.round(price.average_price*100) /100).toFixed(2)}</ListGroup.Item>
-                              }
-                            })
-                          }
-                        </ListGroup>
-                      </Carousel.Item>
-                      <Carousel.Item>
-                        <h3>Rent <i class="fas fa-home"></i></h3>
-                        <ListGroup className="px-5" variant="flush">
-                        {
-                            prices.map(price => {
-                              if(price.item_name === "Apartment (1 bedroom) Outside of Centre, Rent Per Month" ||
-                              price.item_name === "Apartment (1 bedroom) in City Centre, Rent Per Month" ||
-                              price.item_name === "Basic (Electricity, Heating, Cooling, Water, Garbage) for 85m2 Apartment, Utilities (Monthly)"||
-                              price.item_name === "Apartment (3 bedrooms) in City Centre, Rent Per Month"||
-                              price.item_name === "Apartment (3 bedrooms) Outside of Centre, Rent Per Month"||
-                              price.item_name === "Internet (60 Mbps or More, Unlimited Data, Cable/ADSL), Utilities (Monthly)"
-                              ) {
-                                return <ListGroup.Item>{price.item_name} {(Math.round(price.average_price*100) /100).toFixed(2)}</ListGroup.Item>
-                              }
-                            })
-                          }
-                        </ListGroup>
-                      </Carousel.Item>
-                      <Carousel.Item>
-                        <h3>Transportation <i class="fas fa-shopping-cart"></i></h3>
-                        <ListGroup className="px-5" variant="flush">
-                          {
-                            prices.map(price => {
-                              if(
-                              price.item_name === "One-way Ticket (Local Transport), Transportation"||
-                              price.item_name === "Monthly Pass (Regular Price), Transportation"||
-                              price.item_name === "Gasoline (1 liter), Transportation"||
-                              price.item_name === "Volkswagen Golf 1.4 90 KW Trendline (Or Equivalent New Car), Transportation"
-
-                              ) {
-                                return <ListGroup.Item>{price.item_name} {(Math.round(price.average_price*100) /100).toFixed(2)}</ListGroup.Item>
-                              }
-                            })
-                          }
-                        </ListGroup>
-                      </Carousel.Item>
-                    </Carousel>
-                  </Col>
-                </Row>
-                {/* <Row>
-                  <Col className="d-flex justify-content-center align-items-center flex-column pt-2">
+                  <Col className="d-flex align-items-center flex-column pt-2">
                     <h3>Groceries <i class="fas fa-shopping-cart"></i></h3>
                     <ListGroup className="px-2" variant="flush">
                       {
@@ -154,7 +154,7 @@ const UniversityPage = () => {
                       }
                     </ListGroup>
                   </Col>
-                  <Col className="d-flex justify-content-center align-items-center flex-column pt-2">
+                  <Col className="d-flex align-items-center flex-column pt-2">
                     <h3>Rent <i class="fas fa-home"></i></h3>
                     <ListGroup className="px-2" variant="flush">
                     {
@@ -174,7 +174,7 @@ const UniversityPage = () => {
                       }
                     </ListGroup>
                   </Col>
-                  <Col className="d-flex justify-content-center align-items-center flex-column pt-2">
+                  <Col className="d-flex align-items-center flex-column pt-2">
                     <h3>Transportation <i class="fas fa-shopping-cart"></i></h3>
                     <ListGroup className="px-2" variant="flush">
                       {
@@ -192,7 +192,7 @@ const UniversityPage = () => {
                       }
                     </ListGroup>
                   </Col>
-                </Row> */}
+                </Row>
               </Container>
             </Col>
           </Row>
